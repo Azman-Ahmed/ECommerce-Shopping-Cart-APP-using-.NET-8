d391e5bf43a9f460ff95ca98bd4752ba76943523
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-06-17 16:38:41 +0600

    Ecommerce Project 1.0 Product Class Added

---
+++ ECommerce/ECommerce/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.Models
+{
+    public class Product
+    {
+        public required String Id { get; set; }
+        public required String Name { get; set; }
+        public Double Price { get; set; }
+        public required String Photo { get; set; }
+
+
+    }
+}
